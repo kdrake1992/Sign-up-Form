c24ef3f1eb5a3603e4bcfe42d094ad92ab4accc7
kdrake1992 <kdrake1992@outlook.com>
2022-04-28 11:23:48 +0900

    Added some notes.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Checks to see if the password is the same in "confirm password."
 function onChange() {
     const password = document.querySelector('input[name=password]');
     const confirm = document.querySelector('input[name=password_check]');
